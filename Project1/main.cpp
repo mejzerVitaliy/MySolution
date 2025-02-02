789c9948c0efe532bbef24e6bec5a45ca918795d
mejzerVitaliy <vitalii.mejzer@lumitech.co>
2025-02-02 23:04:45 +0200

    second commit

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>;
+#include <iostream> 
+#include <Windows.h> 
 
-int main() {
+int main()
+{
+	//std::cout << "Hello World!" << std::endl; 
+	//std::cout << std::endl;
+	//std::cout << "i do not like c++";  
+
+	// /n
+
+	setlocale(LC_ALL, "rus");
+
+	SetConsoleCP(1251); 
+	SetConsoleOutputCP(1251);
+
+
+	std::cout << "Привіт, світ!\n";
+	std::cout << "Привет, мир!\n";
+
+	//гнгкенгкне
+
 	return 0;
 }
