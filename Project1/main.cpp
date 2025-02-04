80c0ca3955be332391b8338f33ba323951a9161b
mejzerVitaliy <vitalii.mejzer@lumitech.co>
2025-02-04 23:36:15 +0200

    fourth lesson

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,130 +1,107 @@
 #include <iostream> 
 
-int main() {
-    /*long double
-    double
-    float
-    unsigned long long
-    long long
-    unsigned int
-    int
-    char, bool, wchar_t, char_16t, char_32_t, short, unsigned short  -> int*/
+int main()
+{
+	// Синтаксис оператора if: (28) 
+
+	 /*if (умова№1 )
+	  дія№1
+	 else
+	  дія№2
+	   */
+
+	   //std::cout << "Enter a number"; 
+	   //int number; 
+	   //std::cin >> number; 
+
+	   //if (number == 1) 
+	   // std::cout << "One.\n"; 
+	   //else if (number == 2) 
+	   // std::cout << "Two.\n"; 
+	   //if (number == 3) 
+	   // std::cout << "Three.\n"; 
 
 
 
-    //int a = 2; 
-    //char b = 'A'; 
-    //float c = 1.1F; 
-    //double d = 2.2; 
-    // 
-    //std::cout << a + b + c + d; 
 
-    // 1. (С) -> (Тип)Значення 
-    // 2. (С++) -> Тип(Значення) 
-    // 3. (С++) -> ststic_cast<Тип>(Значення) 
+	   //ТЕРНАРИЙ ОПЕРАТОР(29) 
+	   //?:  
+	   // Умова ? Вираз1 : Вираз2 
 
-    //int side1 = 3, side2 = 4; 
+	   //std::cout << "Enter two numbers: "; 
+	   //int number1, number2; 
+	   //std::cin >> number1 >> number2; 
 
-    //// 1/ double division = (double)side1 / side2; 
-    // // 2/ double division = double(side1) / side2; 
-    //double division = static_cast<double>(side1) / side2; 
+	   //int max = (number1 > number2 ? number1 : number2); 
 
-    //std::cout << division << std::endl; 
-
-    //sizeof (25) 
-
-    //sizeof(Тип_даних) 
-    //sizeof(Вираз/Значення) 
-    //sizeof Вираз/Значення) 
+	   //std::cout << max << std::endl; 
 
 
-    /*std::cout << "--- Characters --- \n";
-    std::cout << "char:                " << sizeof(char) << std::endl;
-    std::cout << "wchar_t:             " << sizeof(wchar_t) << std::endl;
-    std::cout << "char16_t             " << sizeof(char16_t) << std::endl;
-    std::cout << "char32_t:            " << sizeof(char32_t) << std::endl;
+	   //SWITCH(30) 
 
-    std::cout << std::endl;
-
-    std::cout << "--- Integers --- \n";
-    std::cout << "short:               " << sizeof(short) << std::endl;
-    std::cout << "unsigned short:      " << sizeof(unsigned short) << std::endl;
-    std::cout << "int:                 " << sizeof(int) << std::endl;
-    std::cout << "unsigned int:        " << sizeof(unsigned) << std::endl;
-    std::cout << "long:                " << sizeof(long) << std::endl;
-    std::cout << "unsigned long:       " << sizeof(unsigned long) << std::endl;
-    std::cout << "long long:           " << sizeof(long long) << std::endl;
-    std::cout << "unsigned long long:  " << sizeof(unsigned long long) << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "--- Floats --- \n";
-    std::cout << "float:               " << sizeof(float) << std::endl;
-    std::cout << "double:              " << sizeof(double) << std::endl;
-    std::cout << "long double:         " << sizeof(long double) << std::endl;*/
+	   /*switch(ім'я_змінної)
+	   {
+		case Значення №1:
+		 Перелік_дій №1
+		 break;
+		case Значення №2:
+		 Перелік_дій №2
+		 break;
+		case Значення №3:
+		 Перелік_дій №3
+		 break;
+		case Значення №4:
+		 Перелік_дій №4
+		 break;
+		default:
+		 Перелік_Дій_За_Замовчуванням
+		 break;
+		}*/
 
 
-    //int number = 10; 
+		//(31)ПОНЯТТЯ ЦИКЛУ 
 
-    //std::cout << sizeof number << std::endl; 
-    //std::cout << sizeof (number + 3) << std::endl; 
-
-    // >, <, >=, <=, ==, !=   Повертають значення типу bool(true/false) 
-
-    //double a = 1.0 / 3; 
-    //double b = 2.0 / 6; 
-
-    //std::cout << a << " " << b << std::endl; 
-
-    //bool expression = (a >= b); 
-
-    //std::cout << expression; 
-
-    // ||, &&, !   
-
-
-    //'і' -> && 
-
-    // умова1 && умова2 
-    // 1. true && true -> true 
-    // 2. true && false -> false 
-    // 3. false && true -> false 
-    // 4. false && false -> false 
-
-    /*std::cout << "Enter a number: ";
-    int number;
-    std::cin >> number;
-
-    bool expression = (2 < number && number < 5);
-
-    std::cout << expression;*/
+		// 1. while 
+		// 2. do while 
+		// 3. for (стандартна форма) 
+		// 4. for з діапазоном 
 
 
 
-    // Логічне 'або' -> || 
-    // умова1 || умова2 
-    // 1. true || true -> true 
-    // 2. true || false -> true 
-    // 3. false || true -> true 
-    // 4. false || false -> false 
+		//(32) Синтаскис циклу while: коли дія зв'язана з умовою 
 
-       /*std::cout << "Enter a number: ";
-    int number;
-    std::cin >> number;
-
-    bool expression = ((number == 10) || (number == 15));
-
-    std::cout << expression;*/
-
-    // Логічне 'не' -> ! 
-    // ! умова 
-    // 1. !true = false 
-    // 2. !false = true 
+		/*while (умова)
+		{
+		Перелік дій
+		}
+		*/
 
 
-    bool expression = (!strcmp("Hello", "hello"));
+		/*int sum = 0;
+		int number;
 
-    std::cout << expression;
+		std::cout << "Enter a number (or 0 to stop) ";
+		std::cin >> number;
+		sum += number;
 
-    return 0;
+		while (number != 0)
+		{
+		std::cout << "Enter a number (or 0 to stop) ";
+		std::cin >> number;
+		sum += number;
+		}
+
+		std::cout << "Sum = " << sum << std::endl;*/
+
+
+
+		//(33) Синтаскис циклу do while: коли дія зв'язана з умовою 
+
+	   /*do
+	   {
+	   Перелік дій
+		}while (умова);
+	   */
+
+	return 0;
 }
